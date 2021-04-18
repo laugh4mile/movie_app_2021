0aadf9b9ad5b052567b600867333a028988a9cca
Hyunsoo <laugh4mile@naver.com>
2021-04-19 01:57:27 +0900

    feat: learn React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 import React from 'react';
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name} />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'kimch',
+    image: 'https://cdn.imweb.me/thumbnail/20200415/6b6e035658bac.png',
+  },
+  {
+    id: 2,
+    name: 'ramen',
+    image:
+      'https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg',
+  },
+  {
+    id: 3,
+    name: 'samgyepsal',
+    image: 'https://cdn.banhanu.com/images/1000000145_5.jpg',
+  },
+];
+
 function App() {
-  return <div>Hello!!!!!!!!</div>;
+  return (
+    <div>
+      {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
