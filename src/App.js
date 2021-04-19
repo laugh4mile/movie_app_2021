d3c6b6b5e98b0d3ebe4eb62a16afd39b8909897a
Hyunsoo <laugh4mile@naver.com>
2021-04-19 23:41:47 +0900

    feat: react propTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
       <img src={picture} alt={name} />
     </div>
   );
 }
+
+Food.sexyTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.string,
+};
 
 const foodILike = [
   {
@@ -20,11 +28,13 @@ const foodILike = [
     name: 'ramen',
     image:
       'https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg',
+    rating: 4.3,
   },
   {
     id: 3,
     name: 'samgyepsal',
     image: 'https://cdn.banhanu.com/images/1000000145_5.jpg',
+    rating: 4.8,
   },
 ];
 
@@ -32,7 +42,12 @@ function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
